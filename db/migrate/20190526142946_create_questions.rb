ae1cabdde8ce246eacd94c11800e9d70034ba9c4
yujiloh0816 <imp.yfujiwara@gmail.com>
2019-05-26 23:38:11 +0900

    Add seeds

--- db/migrate/20190526142946_create_questions.rb
+++ db/migrate/20190526142946_create_questions.rb
@@ -1,7 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
-      t.references :survey
+      t.integer :survey_id
       t.text :content
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 2019_05_26_143025) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -25,7 +24,6 @@ ActiveRecord::Schema.define(version: 2019_05_26_143025) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["survey_id"], name: "index_questions_on_survey_id"
   end
 
   create_table "surveys", force: :cascade do |t|
