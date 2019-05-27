a0c712b2fdabec1d1fcae461d4395a9aa4799e96
yujiloh0816 <imp.yfujiwara@gmail.com>
2019-05-27 13:05:23 +0900

    Remove turbolinks

--- app/assets/javascripts/surveys.js
+++ app/assets/javascripts/surveys.js
@@ -1,38 +1,26 @@
-// (function () {
-//   alert("hogehoge");
-// })();
-//
-// $(document).ready(function () {
-//   $('form').on('click', '.remove_fields', function(e){
-//     (function () {
-//       alert("remove-hogehoge");
-//     })();
-//
-//     // this = link_to のボタン
-//     // thisのひとつ前(prev)のinput[type=hidden]要素を取得し1をvalueとする
-//     $(this).prev('input[type=hidden]').val('1');
-//     // thisの近いfieldsetタグを隠す
-//     $(this).closest('fieldset').hide();
-//     e.preventDefault();
-//   });
-//
-// });
-//
-// $(document).ready(function () {
-//   $('form').on('click', '.add_fields', function(e){
-//     (function () {
-//       alert("add-hogehoge");
-//     })();
-//
-//     // time変数に代入　現在の時間を作成して、時間だけを取り出す
-//     time = new Date().getTime();
-//     // regexpに代入
-//     // 正規表現を作成　$thisのid属性を取得
-//     // 「g」フラグにによるグローバルサーチ
-//     regexp = new RegExp($(this).data('id'), 'g');
-//     // before(content) 各要素の前にコンテンツを挿入する。
-//     // thisの要素data-field要素.regexp
-//     $(this).before($(this).data('fields').replace(regexp, time));
-//     e.preventDefault();
-//   });
-// });
+$(document).ready(function () {
+  $('form').on('click', '.remove_fields', function(e){
+    // this = link_to のボタン
+    // thisのひとつ前(prev)のinput[type=hidden]要素を取得し1をvalueとする
+    $(this).prev('input[type=hidden]').val('1');
+    // thisの近いfieldsetタグを隠す
+    $(this).closest('fieldset').hide();
+    e.preventDefault();
+  });
+
+});
+
+$(document).ready(function () {
+  $('form').on('click', '.add_fields', function(e){
+    // time変数に代入　現在の時間を作成して、時間だけを取り出す
+    time = new Date().getTime();
+    // regexpに代入
+    // 正規表現を作成　$thisのid属性を取得
+    // 「g」フラグにによるグローバルサーチ
+    regexp = new RegExp($(this).data('id'), 'g');
+    // before(content) 各要素の前にコンテンツを挿入する。
+    // thisの要素data-field要素.regexp
+    $(this).before($(this).data('fields').replace(regexp, time));
+    e.preventDefault();
+  });
+});
